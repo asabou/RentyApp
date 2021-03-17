a5e6d485771d5af7d2940fd62dab7966776854b9
asabou <alexmihai906@gmail.com>
2021-03-17 22:44:43 +0200

    seted prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   prod: true,
-  stripePublishableKey: "",
+  serverUrl: "http://renty.go.ro:8080",
+  stripePublishableKey: "pk_test_51IMvgyJcxq1wroO7RHpy6puCkeceDfydJfMw8xPK4554rQbL6Iz51iZ0rMIZ5PVD8esVbpVlDg0o6dKaeVWcPTg000CQB3zlt0",
   name: "prod"
 };
